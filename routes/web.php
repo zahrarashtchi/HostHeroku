055fba345262f86160d92367008b8762095b34aa
zahrarashtchi <78696442+zahrarashtchi@users.noreply.github.com>
2021-04-17 22:15:44 -0700

    truncating the database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    User::truncate();
 });
 
 Auth::routes(['verify' => true]);
