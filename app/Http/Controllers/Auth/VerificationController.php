5bbd7346db21c82ff952401d7f5f6c3d9505aafc
zahrarashtchi <78696442+zahrarashtchi@users.noreply.github.com>
2021-04-19 00:24:53 -0700

    make it better

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
+
 
 class VerificationController extends Controller
 {
