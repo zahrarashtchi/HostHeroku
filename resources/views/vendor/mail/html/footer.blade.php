0c3b18528c52c8f991a6c4435919cbf0a9189173
zahrarashtchi <78696442+zahrarashtchi@users.noreply.github.com>
2021-04-23 23:44:44 -0700

    soooooo

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -3,8 +3,7 @@
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
 <td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
-{{ 'علیک در html فوتر'}}
+
 </td>
 </tr>
 </table>
